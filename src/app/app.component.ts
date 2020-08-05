c39d821c89334374cfbc14b1a9245d67bbdd590b
David Galeano <davidgaleano2611@gmail.com>
2020-08-04 22:40:21 -0500

    Estos cambios son todo el Login

---
+++ src/app/app.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent implements OnInit {
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Inicio',
+      url: '/folder/Inicio',
+      icon: 'home'
+    },
+    {
+      title: 'Quienes somos',
+      url: '/folder/Quienes Somos',
+      icon: 'bulb'
+    },
+    {
+      title: 'Estudiantes',
+      url: '/folder/Estudiantes',
+      icon: 'book'
+    },
+    {
+      title: 'Egresados',
+      url: '/folder/Egresados',
+      icon: 'school'
+    },
+    {
+      title: 'Docentes',
+      url: '/folder/Docentes',
+      icon: 'briefcase'
+    },
+    {
+      title: 'Investigacion',
+      url: '/folder/Investigacion',
+      icon: 'folder-open'
+    },
+    {
+      title: 'Administrativos',
+      url: '/folder/Administrativos',
+      icon: 'person'
+    },
+    {
+      title: 'Salir',
+      url: '/folder/Salir',
+      icon: 'exit'
+    }
+  ];
+  public labels = ['Gmail', 'Facebook', 'Instagram', 'YouTube', 'Sitio Web'];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  ngOnInit() {
+    const path = window.location.pathname.split('folder/')[1];
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
+  }
+}
