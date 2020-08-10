71ab92371f49775b78f66fa0a33b5d22338dfba1
David Galeano <davidgaleano2611@gmail.com>
2020-08-10 17:01:08 -0500

    cambios Please enter the commit message for your changes. Lines starting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent implements OnInit {
     },
     {
       title: 'Egresados',
-      url: '/folder/Egresados',
+      url: '/egresados',
       icon: 'school'
     },
     {
