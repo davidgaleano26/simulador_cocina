481c4bc9d20618bbfdfcc6ced3b353aaf4a9aafa
jimmy_alexander <jimmy.calvo@soluciones.software>
2020-08-14 20:34:18 -0500

    Cambios errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginPipe } from './login.pipe';
-<<<<<<< HEAD
 import { GooglePlus } from '@ionic-native/google-plus/ngx';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { firebaseConfig } from '../environments/environment';
 import { AngularFireModule} from '@angular/fire';
-=======
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+//import { ServiceWorkerModule } from '@angular/service-worker';
+//import { environment } from '../environments/environment';
 
->>>>>>> 9daee1797411e436de9c55cb304e9860be6e761d
 @NgModule({
   declarations: [AppComponent, LoginPipe],
   entryComponents: [],
@@ -26,12 +23,9 @@ import { environment } from '../environments/environment';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-<<<<<<< HEAD
     AngularFireAuthModule,
-    AngularFireModule.initializeApp(firebaseConfig)
-=======
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
->>>>>>> 9daee1797411e436de9c55cb304e9860be6e761d
+    AngularFireModule.initializeApp(firebaseConfig),
+    //ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     GooglePlus,
