e928244b742127303c480be9ec52decd9721406f
MikeHGit <112358se@gmail.com>
2020-09-23 17:23:24 -0500

    login con google

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -14,10 +14,7 @@ import * as firebase from 'firebase/app';
 import {AngularFireAuth} from '@angular/fire/auth';
 
 import { BarcodeScannerOptions, BarcodeScanner } from "@ionic-native/barcode-scanner/ngx";
-import * as firebase from 'firebase';
 const { SplashScreen } = Plugins;
-let provider = new firebase.auth.OAuthProvider('microsoft.com');
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -34,7 +31,7 @@ export class LoginPage implements OnInit {
 
   email: string;
   password: string;
-  
+
   // tslint:disable-next-line: max-line-length
   constructor(private barcodeScanner: BarcodeScanner, 
     public router: Router, 
@@ -128,7 +125,6 @@ export class LoginPage implements OnInit {
       return false;
     }
   }
-<<<<<<< HEAD
  
   async login() {
     const isIE = await window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
@@ -139,18 +135,6 @@ export class LoginPage implements OnInit {
       this.auhService.loginRedirect({
         extraScopesToConsent: ['user.read', 'openid', 'profile']
       });
-=======
- }
-// async login() {
-//     const isIE = await window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
-//    const requestObj = {
-//      scopes: ['user.read']
-//    };
-//    if (isIE) {
-//      this.auhService.loginRedirect({
-//        extraScopesToConsent: ['user.read', 'openid', 'profile']
-//      });
->>>>>>> d585a20d0eec14fb5dc682cede119c1524fadb96
       // tslint:disable-next-line: only-arrow-functions
       // this.auhService.acquireTokenSilent(requestObj).then(function(tokenResponse) {
       //   // Callback code here
@@ -160,7 +144,6 @@ export class LoginPage implements OnInit {
       //   console.log(error);
       // });
       
-<<<<<<< HEAD
     } else {
       this.auhService.loginPopup({
         extraScopesToConsent: ['user.read', 'openid', 'profile']
@@ -175,40 +158,25 @@ export class LoginPage implements OnInit {
       // });
        this.router.navigate(['/inicio']);
     }
-  }
-=======
-//    } else {
-//      this.auhService.loginPopup({
-//        extraScopesToConsent: ['user.read', 'openid', 'profile']
-//      });
-//      // tslint:disable-next-line: only-arrow-functions
-//      // this.auhService.acquireTokenSilent(requestObj).then(function(tokenResponse) {
-//      //   // Callback code here
-//      //   console.log(tokenResponse.accessToken);
-//      // // tslint:disable-next-line: only-arrow-functions
-//      // }).catch(function(error) {
-//      //   console.log(error);
-//      // });
-//       this.router.navigate(['/inicio']);
-//    }
-//}
->>>>>>> d585a20d0eec14fb5dc682cede119c1524fadb96
+}
 
-abrirmicrosoft(){
-  provider.setCustomParameters({
-    auth: {
-      clientId: '77d8101b-ef65-4713-a5f9-a5c73784e382', // This is your client ID
-      authority: 'https://login.microsoftonline.com/organizations',
-      redirectUri: 'https://baselogin-e6cb3.firebaseapp.com/__/auth/handler',// This is your redirect URI
-      postLogoutRedirectUri: "https://baselogin-e6cb3.firebaseapp.com/__/auth/handler"
-    },
-  })
-  firebase.auth().signInWithPopup(provider)
-  .then(function(result){
-    console.log(result)
-  })
-  .catch(function(error){
+LeerCode() {
+  this.barcodeScanner.scan().then(barcodeData => {
+      this.datoscaneado = barcodeData;
+    })
+    .catch(err => {
+      console.log("Error", err);
+    });
+}
 
-  })
+CodificarTexto() {
+  this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE, this.datocodificado).then(
+      encodedData => {
+        this.datocodificado = encodedData;
+      },
+      err => {
+        console.log("Un error ha ocurrido: " + err);
+      }
+    );
 }
 }
